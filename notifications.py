d70fd16893e13a8cefb8db27425c8d7af684f418
JustArist <117675120+justarist@users.noreply.github.com>
2025-12-13 01:18:30 +0300

    Fix formatting and reintroduce notification logic

--- notifications.py
+++ notifications.py
@@ -20,22 +20,22 @@ def format_notification(region: str, attack_type: str, status: str, source: str,
     timestamp = datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S %d-%m-%Y")
     if status == "AC":
         result = f"""
-            <b>✅ ОТБОЙ тревоги</b>\n
-            Регион: {region}\n
-            Тип угрозы: {attack_type}\n
-            Статус: {status}\n
-            Источник: @{source}\n
+            <b>✅ ОТБОЙ тревоги</b>
+            Регион: {region}
+            Тип угрозы: {attack_type}
+            Статус: {status}
+            Источник: @{source}
             Время: {timestamp}
-        """
+            """
     else:
         result = f"""
-            <b>⚠️ ВНИМАНИЕ!</b>\n
-            Угроза {attack_type}\n
-            Регион: {region}\n
-            Уровень: {status}\n
-            Источник: @{source}\n
+            <b>⚠️ ВНИМАНИЕ!</b>
+            Угроза {attack_type}
+            Регион: {region}
+            Уровень: {status}
+            Источник: @{source}
             Время: {timestamp}
-        """
+            """
     if comment:
         result += f"\n\n<i>💬 Комментарий: {comment}</i>"
     return result
