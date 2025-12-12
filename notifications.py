ccccd97fc263684654395f315ded3cb7809b623b
JustArist <117675120+justarist@users.noreply.github.com>
2025-12-13 01:36:06 +0300

    Fix variable names in format_notification function

--- notifications.py
+++ notifications.py
@@ -15,22 +15,22 @@ STATUS_READABLE = {
 }
 
 def format_notification(region: str, attack_type: str, status: str, source: str, comment: str = None) -> str:
-    attack_type = TYPE_LABEL.get(attack_type, attack_type)
-    status = STATUS_READABLE.get(status, status)
+    rattack_type = TYPE_LABEL.get(attack_type, attack_type)
+    rstatus = STATUS_READABLE.get(status, status)
     timestamp = datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S %d-%m-%Y")
     source = f"@{source}" if source != "Admin" else source
     if status == "AC":
         result = (f"<b>✅ ОТБОЙ тревоги</b>\n"
             f"Регион: {region}\n"
-            f"Тип угрозы: {attack_type}\n"
-            f"Статус: {status}\n"
+            f"Тип угрозы: {rattack_type}\n"
+            f"Статус: {rstatus}\n"
             f"Источник: {source}\n"
             f"Время: {timestamp}\n")
     else:
         result = (f"<b>⚠️ ВНИМАНИЕ!</b>\n"
-            f"Угроза {attack_type}\n"
+            f"Угроза {rattack_type}\n"
             f"Регион: {region}\n"
-            f"Уровень: {status}\n"
+            f"Уровень: {rstatus}\n"
             f"Источник: {source}\n"
             f"Время: {timestamp}\n")
     if comment:
